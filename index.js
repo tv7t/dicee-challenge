6a682c441d6b9fa2cd051549b2b291fea3ec3172
tv7t <93339465+tv7t@users.noreply.github.com>
2022-07-06 22:29:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+var image1 = "images/dice"+randomNumber1+".png";
+var image2 = "images/dice"+randomNumber2+".png";
+document.querySelector(".dice .img1").setAttribute("src",image1);
+document.querySelector(".dice .img2").setAttribute("src",image2);
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector(".container h1").textContent = "Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2)
+{
+    document.querySelector(".container h1").textContent = "Player 2 Wins!";
+}
+else{
+    document.querySelector(".container h1").textContent = "Draw!";
+}
